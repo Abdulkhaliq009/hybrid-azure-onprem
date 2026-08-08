534d7447ab58c7f68ab6468a6d2b84ae1390910b
Abdulkhaliq009 <abdulkhaliq24@icloud.com>
2026-08-08 04:16:42 +0200

    fix: main.tf content and appservice variables syntax

--- infra/terraform/modules/appservice/variables.tf
+++ infra/terraform/modules/appservice/variables.tf
@@ -5,4 +5,7 @@ variable "subnet_id"           { type = string }
 variable "db_host"             { type = string }
 variable "db_name"             { type = string }
 variable "db_user"             { type = string }
-variable "db_password"         { type = string sensitive = true }
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
